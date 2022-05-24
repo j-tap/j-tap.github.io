68feafa2a9216aba376286d0251640e7c2fa3140
j-tap <j-tap@ya.ru>
2022-05-24 12:49:29 +0300

    redirect

--- projects/rothmans/static/js/product_animation.js
+++
@@ -1,8 +0,0 @@
-if ( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    $(window).stellar({
-        positionProperty: 'transform',
-        horizontalScrolling: false,
-        hideDistantElements: false,
-    });
-
-}
